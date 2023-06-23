11714b4dcdfd90daab32ef050a46b26c684ed3de
Vincent Clipet <vincent.clipet.7@gmail.com>
2023-06-23 08:56:05 +0100

    fix: removed a leftover console.log()

--- src/prisma-client-exception/prisma-client-exception.filter.ts
+++ src/prisma-client-exception/prisma-client-exception.filter.ts
@@ -6,7 +6,6 @@ import { Response } from 'express';
 @Catch(Prisma.PrismaClientKnownRequestError)
 export class PrismaClientExceptionFilter extends BaseExceptionFilter {
   catch(exception: Prisma.PrismaClientKnownRequestError, host: ArgumentsHost) {
-    console.log(exception)
     const ctx = host.switchToHttp();
     const response = ctx.getResponse<Response>();
     const status = HttpStatus.BAD_REQUEST;
