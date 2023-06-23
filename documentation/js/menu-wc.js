861207fe73292ebac1fd1bcc20db0a423d96bfe8
Vincent Clipet <vincent.clipet.7@gmail.com>
2023-06-23 09:38:25 +0100

    docs: auto-generated documentation

--- documentation/js/menu-wc.js
+++ documentation/js/menu-wc.js
@@ -59,18 +59,18 @@ customElements.define('compodoc-menu', class extends HTMLElement {
                                 <a href="modules/AppModule.html" data-type="entity-link" >AppModule</a>
                                     <li class="chapter inner">
                                         <div class="simple menu-toggler" data-bs-toggle="collapse" ${ isNormalMode ?
-                                            'data-bs-target="#controllers-links-module-AppModule-9a62f4b20a26c14efe4219efd798a47e9dec3ad8928fa94762f73f2b92de9749df3dc48363edd99af6bd26e414aaeb3ca0d30b47c57bcee11ce416dfb6d872c9"' : 'data-bs-target="#xs-controllers-links-module-AppModule-9a62f4b20a26c14efe4219efd798a47e9dec3ad8928fa94762f73f2b92de9749df3dc48363edd99af6bd26e414aaeb3ca0d30b47c57bcee11ce416dfb6d872c9"' }>
+                                            'data-bs-target="#controllers-links-module-AppModule-16e7b6c16d7e4f9ad981b54a1501cec57a579d18bfbc11841a5d6c5178505ea64895d9430af513cb1f043feaa90b61acd5854631bfb2db4e096eed9f68c2a59a"' : 'data-bs-target="#xs-controllers-links-module-AppModule-16e7b6c16d7e4f9ad981b54a1501cec57a579d18bfbc11841a5d6c5178505ea64895d9430af513cb1f043feaa90b61acd5854631bfb2db4e096eed9f68c2a59a"' }>
                                             <span class="icon ion-md-swap"></span>
                                             <span>Controllers</span>
                                             <span class="icon ion-ios-arrow-down"></span>
                                         </div>
-                                        <ul class="links collapse" ${ isNormalMode ? 'id="controllers-links-module-AppModule-9a62f4b20a26c14efe4219efd798a47e9dec3ad8928fa94762f73f2b92de9749df3dc48363edd99af6bd26e414aaeb3ca0d30b47c57bcee11ce416dfb6d872c9"' :
-                                            'id="xs-controllers-links-module-AppModule-9a62f4b20a26c14efe4219efd798a47e9dec3ad8928fa94762f73f2b92de9749df3dc48363edd99af6bd26e414aaeb3ca0d30b47c57bcee11ce416dfb6d872c9"' }>
+                                        <ul class="links collapse" ${ isNormalMode ? 'id="controllers-links-module-AppModule-16e7b6c16d7e4f9ad981b54a1501cec57a579d18bfbc11841a5d6c5178505ea64895d9430af513cb1f043feaa90b61acd5854631bfb2db4e096eed9f68c2a59a"' :
+                                            'id="xs-controllers-links-module-AppModule-16e7b6c16d7e4f9ad981b54a1501cec57a579d18bfbc11841a5d6c5178505ea64895d9430af513cb1f043feaa90b61acd5854631bfb2db4e096eed9f68c2a59a"' }>
                                             <li class="link">
                                                 <a href="controllers/AppController.html" data-type="entity-link" data-context="sub-entity" data-context-id="modules" >AppController</a>
                                             </li>
                                             <li class="link">
-                                                <a href="controllers/FormationController.html" data-type="entity-link" data-context="sub-entity" data-context-id="modules" >FormationController</a>
+                                                <a href="controllers/AuthController.html" data-type="entity-link" data-context="sub-entity" data-context-id="modules" >AuthController</a>
                                             </li>
                                             <li class="link">
                                                 <a href="controllers/LessonController.html" data-type="entity-link" data-context="sub-entity" data-context-id="modules" >LessonController</a>
@@ -85,21 +85,27 @@ customElements.define('compodoc-menu', class extends HTMLElement {
                                                 <a href="controllers/RoleController.html" data-type="entity-link" data-context="sub-entity" data-context-id="modules" >RoleController</a>
                                             </li>
                                             <li class="link">
+                                                <a href="controllers/TrainingController.html" data-type="entity-link" data-context="sub-entity" data-context-id="modules" >TrainingController</a>
+                                            </li>
+                                            <li class="link">
                                                 <a href="controllers/UserController.html" data-type="entity-link" data-context="sub-entity" data-context-id="modules" >UserController</a>
                                             </li>
                                         </ul>
                                     </li>
                                 <li class="chapter inner">
                                     <div class="simple menu-toggler" data-bs-toggle="collapse" ${ isNormalMode ?
-                                        'data-bs-target="#injectables-links-module-AppModule-9a62f4b20a26c14efe4219efd798a47e9dec3ad8928fa94762f73f2b92de9749df3dc48363edd99af6bd26e414aaeb3ca0d30b47c57bcee11ce416dfb6d872c9"' : 'data-bs-target="#xs-injectables-links-module-AppModule-9a62f4b20a26c14efe4219efd798a47e9dec3ad8928fa94762f73f2b92de9749df3dc48363edd99af6bd26e414aaeb3ca0d30b47c57bcee11ce416dfb6d872c9"' }>
+                                        'data-bs-target="#injectables-links-module-AppModule-16e7b6c16d7e4f9ad981b54a1501cec57a579d18bfbc11841a5d6c5178505ea64895d9430af513cb1f043feaa90b61acd5854631bfb2db4e096eed9f68c2a59a"' : 'data-bs-target="#xs-injectables-links-module-AppModule-16e7b6c16d7e4f9ad981b54a1501cec57a579d18bfbc11841a5d6c5178505ea64895d9430af513cb1f043feaa90b61acd5854631bfb2db4e096eed9f68c2a59a"' }>
                                         <span class="icon ion-md-arrow-round-down"></span>
                                         <span>Injectables</span>
                                         <span class="icon ion-ios-arrow-down"></span>
                                     </div>
-                                    <ul class="links collapse" ${ isNormalMode ? 'id="injectables-links-module-AppModule-9a62f4b20a26c14efe4219efd798a47e9dec3ad8928fa94762f73f2b92de9749df3dc48363edd99af6bd26e414aaeb3ca0d30b47c57bcee11ce416dfb6d872c9"' :
-                                        'id="xs-injectables-links-module-AppModule-9a62f4b20a26c14efe4219efd798a47e9dec3ad8928fa94762f73f2b92de9749df3dc48363edd99af6bd26e414aaeb3ca0d30b47c57bcee11ce416dfb6d872c9"' }>
+                                    <ul class="links collapse" ${ isNormalMode ? 'id="injectables-links-module-AppModule-16e7b6c16d7e4f9ad981b54a1501cec57a579d18bfbc11841a5d6c5178505ea64895d9430af513cb1f043feaa90b61acd5854631bfb2db4e096eed9f68c2a59a"' :
+                                        'id="xs-injectables-links-module-AppModule-16e7b6c16d7e4f9ad981b54a1501cec57a579d18bfbc11841a5d6c5178505ea64895d9430af513cb1f043feaa90b61acd5854631bfb2db4e096eed9f68c2a59a"' }>
                                         <li class="link">
                                             <a href="injectables/AppService.html" data-type="entity-link" data-context="sub-entity" data-context-id="modules" >AppService</a>
+                                        </li>
+                                        <li class="link">
+                                            <a href="injectables/AuthService.html" data-type="entity-link" data-context="sub-entity" data-context-id="modules" >AuthService</a>
                                         </li>
                                         <li class="link">
                                             <a href="injectables/PrismaService.html" data-type="entity-link" data-context="sub-entity" data-context-id="modules" >PrismaService</a>
@@ -112,6 +118,88 @@ customElements.define('compodoc-menu', class extends HTMLElement {
                             </li>
                 </ul>
                 </li>
+                        <li class="chapter">
+                            <div class="simple menu-toggler" data-bs-toggle="collapse" ${ isNormalMode ? 'data-bs-target="#controllers-links"' :
+                                'data-bs-target="#xs-controllers-links"' }>
+                                <span class="icon ion-md-swap"></span>
+                                <span>Controllers</span>
+                                <span class="icon ion-ios-arrow-down"></span>
+                            </div>
+                            <ul class="links collapse " ${ isNormalMode ? 'id="controllers-links"' : 'id="xs-controllers-links"' }>
+                                <li class="link">
+                                    <a href="controllers/AppController.html" data-type="entity-link" >AppController</a>
+                                </li>
+                                <li class="link">
+                                    <a href="controllers/AuthController.html" data-type="entity-link" >AuthController</a>
+                                </li>
+                                <li class="link">
+                                    <a href="controllers/LessonController.html" data-type="entity-link" >LessonController</a>
+                                </li>
+                                <li class="link">
+                                    <a href="controllers/ModuleController.html" data-type="entity-link" >ModuleController</a>
+                                </li>
+                                <li class="link">
+                                    <a href="controllers/PermissionController.html" data-type="entity-link" >PermissionController</a>
+                                </li>
+                                <li class="link">
+                                    <a href="controllers/RoleController.html" data-type="entity-link" >RoleController</a>
+                                </li>
+                                <li class="link">
+                                    <a href="controllers/TrainingController.html" data-type="entity-link" >TrainingController</a>
+                                </li>
+                                <li class="link">
+                                    <a href="controllers/UserController.html" data-type="entity-link" >UserController</a>
+                                </li>
+                            </ul>
+                        </li>
+                    <li class="chapter">
+                        <div class="simple menu-toggler" data-bs-toggle="collapse" ${ isNormalMode ? 'data-bs-target="#classes-links"' :
+                            'data-bs-target="#xs-classes-links"' }>
+                            <span class="icon ion-ios-paper"></span>
+                            <span>Classes</span>
+                            <span class="icon ion-ios-arrow-down"></span>
+                        </div>
+                        <ul class="links collapse " ${ isNormalMode ? 'id="classes-links"' : 'id="xs-classes-links"' }>
+                            <li class="link">
+                                <a href="classes/PrismaClientExceptionFilter.html" data-type="entity-link" >PrismaClientExceptionFilter</a>
+                            </li>
+                        </ul>
+                    </li>
+                        <li class="chapter">
+                            <div class="simple menu-toggler" data-bs-toggle="collapse" ${ isNormalMode ? 'data-bs-target="#injectables-links"' :
+                                'data-bs-target="#xs-injectables-links"' }>
+                                <span class="icon ion-md-arrow-round-down"></span>
+                                <span>Injectables</span>
+                                <span class="icon ion-ios-arrow-down"></span>
+                            </div>
+                            <ul class="links collapse " ${ isNormalMode ? 'id="injectables-links"' : 'id="xs-injectables-links"' }>
+                                <li class="link">
+                                    <a href="injectables/AppService.html" data-type="entity-link" >AppService</a>
+                                </li>
+                                <li class="link">
+                                    <a href="injectables/AuthService.html" data-type="entity-link" >AuthService</a>
+                                </li>
+                                <li class="link">
+                                    <a href="injectables/PrismaService.html" data-type="entity-link" >PrismaService</a>
+                                </li>
+                                <li class="link">
+                                    <a href="injectables/UserService.html" data-type="entity-link" >UserService</a>
+                                </li>
+                            </ul>
+                        </li>
+                    <li class="chapter">
+                        <div class="simple menu-toggler" data-bs-toggle="collapse" ${ isNormalMode ? 'data-bs-target="#guards-links"' :
+                            'data-bs-target="#xs-guards-links"' }>
+                            <span class="icon ion-ios-lock"></span>
+                            <span>Guards</span>
+                            <span class="icon ion-ios-arrow-down"></span>
+                        </div>
+                        <ul class="links collapse " ${ isNormalMode ? 'id="guards-links"' : 'id="xs-guards-links"' }>
+                            <li class="link">
+                                <a href="guards/AuthGuard.html" data-type="entity-link" >AuthGuard</a>
+                            </li>
+                        </ul>
+                    </li>
                     <li class="chapter">
                         <div class="simple menu-toggler" data-bs-toggle="collapse" ${ isNormalMode ? 'data-bs-target="#miscellaneous-links"'
                             : 'data-bs-target="#xs-miscellaneous-links"' }>
